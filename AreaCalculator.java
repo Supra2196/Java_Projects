f6d94bef307d23951c52329a07de16ba810d7682
Sudarshan <suprabhu2006@gmail.com>
2025-11-10 10:34:19 -0600

    Implement area calculation methods in AreaCalculator
    Added methods to calculate area for square, rectangle, and circle.

--- AreaCalculator.java
+++ AreaCalculator.java
@@ -2,8 +2,28 @@ import java.util.Scanner;
 
 public class AreaCalculator
 {
+	public static double area(double number_one)
+	{
+		return number_one*number_one;
+	}
+	public static double area(double number_one,double number_two)
+	{
+		return number_one*number_two;
+	}
+	public static double Circlearea(double number_one)
+	{
+		return number_one*number_one*3.14;
+	}
+
     public static void main(String[] args)
     {
-    
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Number:");
+		double numbione=scan.nextInt();
+		System.out.print("Number:");
+		double numbitwo=scan.nextInt();
+		System.out.println("Area of Square:"+area(numbione));
+		System.out.println("Area of Circle:"+Circlearea(numbione));
+		System.out.println("Area of Rectangle:"+area(numbione,numbitwo));
     }
 }
